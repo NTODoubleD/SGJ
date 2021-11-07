f791fb9f4a873e0d0e8798838a6681a89fac5303
DARKozDARKA <kalinkovgoha04@gmail.com>
2021-11-07 09:12:05 +0300

    Something

---
+++ Assets/Scripts/ClientScripts/Effect/TurnOffLight.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffLight : MonoBehaviour
+{
+    [SerializeField] private Light _light;
+    [SerializeField] private float _speed;
+
+    public void Update()
+    {
+        _light.intensity -= _speed * Time.deltaTime;
+    }
+}
